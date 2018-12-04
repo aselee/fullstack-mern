0c4659ad8ed8d6887a08ef4ce87f9c01f62454b8
Andrew <32229265+aselee@users.noreply.github.com>
2018-12-04 15:37:09 -0600

    render

--- src/App.js
+++ src/App.js
@@ -49,11 +49,82 @@ class App extends Component {
 
   // our put method that uses our backend api
   // to create new query into our data base
+  putDataToDB = message => {
+    let currentIds = this.state.data.map(data => data.id);
+    let idToBeAdded = 0;
+    while (currentIds.includes(idToBeAdded)) {
+      ++idToBeAdded;
+    }
+    
+    axios.post("/api/putData", {
+      id: idToBeAdded,
+      message: message
+    });
+  };
 
+  // our delete method that uses our backend api
+  // to remove existing database information
+  deleteFromDB = idTodelete => {
+    let objIdToDelete = null;
+    this.state.data.forEach(dat => {
+      if (dat.id == idTodelete) {
+        objIdToDelete = dat._id;
+      }
+    });
 
+    axios.delete("/api/deleteData", {
+      data: {
+        id: objIdToDelete
+      }
+    });
+  };
 
+  // our update method that uses our backend api
+  // to overwrite existing data base information
+  updateDB = (idToUpdate, updateToApply) => {
+    let objIdToUpdate = null;
+    this.state.data.forEach(dat => {
+      if (dat.id == idToUpdate) {
+        objIdToUpdate = dat._id;
+      }
+    });
+
+    axios.post("/api/updateData", {
+      id: objIdToUpdate,
+      update: { message: updateToApply }
+    });
+  };
+
+  // the UI
   render() {
-    return <div>I'M READY TO USE THE BACK END APIIISSSS!!! :-)</div>;
+    const { data } = this.state;
+
+    return(
+      <div>
+        <ul>
+          {data.length <= 0
+          ? "NO DB ENTRIES AT THE MOMENT"
+          : data.map(dat => (
+            <li style={{ padding: "10px" }} key={data.message}>
+              <span style={{ color: "gray" }}> id: </span> {dat.id} <br />
+              <span style={{ color: "gray" }}> data: </span>
+                {dat.message}
+            </li>
+          ))}
+        </ul>
+        <div style={{ padding: "10px" }}>
+          <input
+            type="text"
+            onChange={e => this.setState({ message: e.target.value})}
+            placeholder="add somethinggg in the databaseee"
+            style={{ width: "200px"}}
+          />
+          <button onClick={() => this.putDataToDB(this.state.message)}>
+            Add
+          </button>
+        </div>
+      </div>
+    )
   }
 }
 
