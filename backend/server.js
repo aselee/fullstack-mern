409fc17d867e03936954edf662278ed974229d06
Andrew <32229265+aselee@users.noreply.github.com>
2018-12-03 18:39:18 -0600

    adding backend server code

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,82 @@
-//testing
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const Data = require('./data');
 
-// testing
+const API_PORT = 3001;
+const app = express();
+const router = express.Router();
+
+// this is our MongDb database
+const dbRoute = "mongodb://jelo:a9bc839993@ds151382.mlab.com:51382/jelotest";
+
+// connects our back end code with the database
+mongoose.connect(
+  dbRoute,
+  { useNewUrlParser: true }
+);
+
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+
+// checks if connection with the database is successful
+db.on("error", console.error.bind(console, "MongoDB conection error:"));
+
+//(optional) only made for logging and 
+// bodyParser, parses the request body to be a readable json format
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger("dev"));
+
+// Get method
+// this method fetches all available data in our database
+router.get("/getData", (req, res) => {
+  Data.find((err, data) => {
+    // if error returns false, show as error
+    // return true if data is successfully fetched
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: data });
+  });
+});
+
+// Update method
+// this method overwrites existing data in our database
+router.post("/updateData", (req, res) => {
+  const { id, update } = req.body;
+  Data.findOneAndUpdate(id, update, err => {
+    // if error return false
+    // return updates if true
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+// Delete method
+// this method removes exisiting data in our database
+router.delete("/deleteData", (req, res) => {
+  const { id } = req.body;
+  Data.findOneAndDelete(id, err => {
+    if (err) return res.send(err);
+    return res.json({ success: true});
+  });
+});
+
+// Create method
+// this method adds new data in our database
+router.post("/putData", (req, res) => {
+  let data = new Data();
+
+  const { id, message } = req.body;
+
+  if ((!id && id !==0) || !message) {
+    return res.json({
+      success: false,
+      error: "INVALID INPUTS"
+    });
+  }
+  data.message = message;
+  data.id = id;
+});
